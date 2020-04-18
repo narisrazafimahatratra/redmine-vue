7d755e8159f97cbfb04940b627ecf0043d1b9607
narisrazafimahatratra <63517825+narisrazafimahatratra@users.noreply.github.com>
2020-04-18 23:57:30 +0300

    Add TaskCardItem.vue

---
+++ components/kanban/TaskCardItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card
+    max-width=400
+  >
+    <v-card-title>
+      <v-list-item class="grow">
+        <v-row
+            align="center"
+          >
+          <v-chip
+            class="ma-2"
+            color=primary
+            label
+            text-color="white"
+          >
+            <v-icon left>mdi-alpha-{{ ticketType }}-box-outline</v-icon>
+            {{ ticketId }}
+          </v-chip>
+          <v-chip
+            class="ma-2"
+            color="red"
+            v-for="tag in tags"
+            :key="tag"
+          >
+            {{ tag }}
+          </v-chip>
+        </v-row>
+      </v-list-item>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-bold">
+      {{ ticketTitle }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            :alt="assignee.name"
+            :src="assignee.avatar"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-row
+          align="center"
+          justify="end"
+        >
+          <v-icon class="mr-1">mdi-calendar-arrow-right</v-icon>
+          <span class="subheading mr-2">{{ $moment(startDate).format("DD MMMM YYYY") }}</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1">mdi-calendar-arrow-left</v-icon>
+          <span class="subheading mr-2">{{ $moment(endDate).format("DD MMMM YYYY") }}</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1">mdi-progress-check</v-icon>
+          <span class="subheading">{{ estimation }}h</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      ticketId: 15966,
+      ticketTitle : 'Enquête - Script : changer icônes',
+      ticketType: 'd',
+      tags : ['Critique', 'Demo'],
+      assignee: {
+        name: 'Naris Razafimahatratra',
+        avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'
+      },
+      startDate: '04-05-2020',
+      endDate: '04-05-2020',
+      estimation: 8
+    }
+  }
+}
+
+</script>
---
+++ pages/kanban.vue
@@ -0,0 +1,13 @@
+<template>
+  <TaskCardItem></TaskCardItem>
+</template>
+
+<script>
+import TaskCardItem from '~/components/kanban/TaskCardItem.vue'
+
+export default {
+  components: {
+    TaskCardItem,
+  }
+}
+</script>
